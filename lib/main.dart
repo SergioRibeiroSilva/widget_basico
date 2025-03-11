8d920469dcc6352a6353d6be0777b976d2abe684
sergio <pastorsergioribeiro@gmail.com>
2025-03-11 13:10:38 -0300

    inseri comentário

--- lib/main.dart
+++ lib/main.dart
@@ -9,12 +9,15 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   // This widget is the root of your application.
+  //inseri comentário embaixo.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo alterei no git',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.deepPurple,
+        ),
       ),
       home: MyHome(),
     );

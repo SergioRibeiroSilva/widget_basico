2f80e24725666796c94b172e09e78522f668dfa1
sergioribeirosilva <pastorsergioribeiro@gmail.com>
2025-03-10 23:06:09 -0300

    Primeiro widget scaffold

---
+++ lib/widget_scaffold.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class WidgetScaffold extends StatelessWidget {
+  const WidgetScaffold({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Minha aplicação')),
+      body: Center(
+        child: Text('Olá! Alterando.', style: TextStyle(fontSize: 50)),
+      ),
+      floatingActionButton: FloatingActionButton(onPressed: () {}),
+    );
+  }
+}

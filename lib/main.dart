c0652202624655425c128f70fd89b1d63cdb9270
sergio <pastorsergioribeiro@gmail.com>
2025-03-14 18:21:45 -0300

    Computador de baixo 14/03 18:21

--- lib/main.dart
+++ lib/main.dart
@@ -11,13 +11,64 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "Coleção de widgets",
       theme: ThemeData(primaryColor: Colors.blue),
-      home: WidgetCorPreferida(),
+      home: BuildListView(),
     );
   }
 }
+
+class BuildListView extends StatefulWidget {
+  const BuildListView({super.key});
+
+  @override
+  State<BuildListView> createState() =>
+      _BuildListViewState();
+}
+
+class _BuildListViewState extends State<BuildListView> {
+  //var users = new List<User>;
+
+  List<User> users = [];
+
+  _getUsers() {
+    API.getUsers().then((response) {
+      //Atualiza o layout
+      setState(() {
+        Iterable lista = json.decode(response.body);
+        users =
+            lista
+                .map((model) => User.fromJson(model))
+                .toList();
+      });
+    });
+  }
+
+  _BuildListViewState() {
+    _getUsers();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Lista de usuários')),
+      body: listaUsuarios(),
+    );
+  }
+
+  listaUsuarios() {
+    return ListView.builder(
+      itemCount: users.length,
+      itemBuilder: (context, index) {
+        return ListTile(
+          title: Text(users[index].name.toString()),
+        );
+      },
+    );
+  }
+}
+
+//https://jsonplaceholder.typicode.com/users

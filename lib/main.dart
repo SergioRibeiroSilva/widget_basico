b9eaed417ec0a723eb5164c9a4fe32893250c9a2
sergioribeirosilva <pastorsergioribeiro@gmail.com>
2025-03-15 22:49:24 -0300

    Notebook 22:43 em 15/03

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
-import 'package:widget_basico/api_chuck.dart';
+import 'package:widget_basico/widget_listview_chuck.dart';
 
 void main() {
   runApp(AplicacaoPadrao());
@@ -19,39 +17,3 @@ class AplicacaoPadrao extends StatelessWidget {
     );
   }
 }
-
-class BuildListViewChuck extends StatefulWidget {
-  const BuildListViewChuck({super.key});
-  @override
-  State<BuildListViewChuck> createState() => _BuildListViewChuckState();
-}
-
-class _BuildListViewChuckState extends State<BuildListViewChuck> {
-  String valor = "";
-
-  _getChuck() {
-    API_chuck.getChuck().then((response) {
-      setState(() {
-        var res = json.decode(response.body);
-        valor = res['value'];
-      });
-    });
-  }
-
-  _BuildListViewChuckState() {
-    _getChuck();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Chucks.....')),
-      body: Padding(
-        padding: const EdgeInsets.all(18.0),
-        child: Text(valor.toString(), style: TextStyle(fontSize: 30)),
-      ),
-    );
-  }
-}
-
-//uma observação
